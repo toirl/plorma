1d6abeee1c3ab008311a848b72b07b6344b441fa
Torsten Irländer <torsten.irlaender@intevation.de>
2015-03-09 23:23:46 +0100

    Renamed files

--- setup.py
+++ setup.py
@@ -6,8 +6,8 @@ import os
 from setuptools import setup, find_packages
 
 here = os.path.abspath(os.path.dirname(__file__))
-README = open(os.path.join(here, 'README.txt')).read()
-CHANGES = open(os.path.join(here, 'CHANGES.txt')).read()
+README = open(os.path.join(here, 'README')).read()
+CHANGES = open(os.path.join(here, 'CHANGES')).read()
 
 # Feature release
 version_increment = '0.1'
