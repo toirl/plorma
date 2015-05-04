f9649e6ec59a7a87ee275b67bc017447c4e56dad
Torsten Irländer <torsten@irlaender.de>
2015-05-04 12:56:43 +0200

    Added fields to the sprint modul

--- plorma/model/sprint.py
+++ plorma/model/sprint.py
@@ -9,3 +9,6 @@ class Sprint(BaseItem, Owned, Base):
     __tablename__ = 'sprints'
     _modul_id = 1003
     id = sa.Column(sa.Integer, primary_key=True)
+    start = sa.Column('start', sa.Date)
+    end = sa.Column('end', sa.Date)
+    strength = sa.Column('strength', sa.Integer)
