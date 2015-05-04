d09b79b46641d77c4e7510754db34a476ee9e92a
Torsten Irländer <torsten@irlaender.de>
2015-05-04 23:10:13 +0200

    Added delete callback and fixed updatecallback to update the estimate log

--- plorma/views/tasks.py
+++ plorma/views/tasks.py
@@ -1,6 +1,6 @@
 import datetime
 from pyramid.view import view_config
-from ringo.views.base import create, update
+from ringo.views.base import create, update, delete
 from ringo.lib.helpers import get_action_routename
 
 from plorma.model.task import Task
@@ -10,17 +10,20 @@ from plorma.model.sprint import Estimatelog
 def create_callback(request, task):
     return update_callback(request, task)
 
+def delete_callback(request, task):
+    return update_callback(request, task)
 
 def update_callback(request, task):
-    for sprint in task.sprints:
-        for log in sprint.estimatelog:
-            if log.date == datetime.date.today():
-                log.estimate = sprint.estimate
-                return task
-        nlog = Estimatelog()
-        nlog.estimate = sprint.estimate
-        nlog.date = datetime.date.today()
-        sprint.estimatelog.append(nlog)
+    for ptask in task.get_parents():
+        for sprint in ptask.sprints:
+            for log in sprint.estimatelog:
+                if log.date == datetime.date.today():
+                    log.estimate = sprint.estimate
+                    break
+            nlog = Estimatelog()
+            nlog.estimate = sprint.estimate
+            nlog.date = datetime.date.today()
+            sprint.estimatelog.append(nlog)
     return task
 
 
@@ -35,3 +38,9 @@ def create_(request):
              permission='update')
 def update_(request):
     return update(request, update_callback)
+
+@view_config(route_name=get_action_routename(Task, 'delete'),
+             renderer='/default/confirm.mako',
+             permission='delete')
+def delete_(request):
+    return delete(request, delete_callback)
