4cc452bf76f4ea3aa07354fb72e26fe9e53605a3
Torsten Irländer <torsten@irlaender.de>
2015-10-24 15:37:01 +0200

    Set version to 0.3

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ requires = [
 ]
 
 setup(name='plorma',
-      version='0.2',
+      version='0.3',
       description=('Plorma is a task management application to plan '
                    'organise and manage your tasks.'),
       long_description=README + '\n\n' + CHANGES,
